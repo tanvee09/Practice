2a32397415d46e247230a7b83931dc89e0aa39b8
Tanvee Balhara <balharatanvee@gmail.com>
2020-12-19 12:56:31 +0530

    corrected code

--- possibleToReach.cpp
+++ possibleToReach.cpp
@@ -5,35 +5,29 @@
 using namespace std;
 
 bool isPathPossible(vector<vector<int>> matrix, vector<vector<int>> visited, int n, int m, int i, int j) {
-    if (i >= n || j >= m || visited[i][j])
+    if (i >= n || j >= m || i < 0 || j < 0 || visited[i][j])
         return false;
     if (i == n - 1 && j == m - 1)
         return true;
-    if (i + 1 < n && matrix[i][j] >= matrix[i + 1][j] && isPathPossible(matrix, visited, n, m, i + 1, j)) {
-        visited[i + 1][j] = true;
-        return true;
-    }
-    if (i - 1 >= 0 && matrix[i][j] >= matrix[i - 1][j] && isPathPossible(matrix, visited, n, m, i - 1, j)) {
-        visited[i - 1][j] = true;
-        return true;
-    }
-    if (j + 1 < m && matrix[i][j] >= matrix[i][j + 1] && isPathPossible(matrix, visited, n, m, i, j + 1)) {
-        visited[i][j + 1] = true;
-        return true;
-    }
-    if (j - 1 >= 0 && matrix[i][j] >= matrix[i][j - 1] && isPathPossible(matrix, visited, n, m, i, j - 1)) {
-        visited[i][j - 1] = true;
-        return true;
-    }
+    visited[i][j] = true;
+    if (i + 1 < n && matrix[i][j] >= matrix[i + 1][j] && isPathPossible(matrix, visited, n, m, i + 1, j))
+            return true;
+    if (i - 1 >= 0 && matrix[i][j] >= matrix[i - 1][j] && isPathPossible(matrix, visited, n, m, i - 1, j))
+            return true;
+    if (j + 1 < n && matrix[i][j] >= matrix[i][j + 1] && isPathPossible(matrix, visited, n, m, i, j + 1))
+            return true;
+    if (j - 1 >= 0 && matrix[i][j] >= matrix[i][j - 1] && isPathPossible(matrix, visited, n, m, i, j - 1))
+            return true;
+    visited[i][j] = false;
     return false;
 }
 
 int main()
 {
     int n = 5, m = 4;
-    vector<vector<int>> matrix = {{25, 24, 23, 22},
-                                  {21, 20, 19, 18},
-                                  {17, 16, 15, 14},
+    vector<vector<int>> matrix = {{25, 25, 40, 22},
+                                  {25, 25, 19, 18},
+                                  {40, 40, 40, 14},
                                   {13, 12, 11, 10},
                                   { 9,  8,  7,  6}};
     vector<vector<int>> visited(n, vector<int>(m, false));
